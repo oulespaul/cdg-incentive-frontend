fc99ff643bda64637ca4e9c1fcc7f8ec72de81fd
Thanakrit <tanapol@Thanakrits-MacBook-Pro.local>
2024-08-12 19:21:38 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>CDG Incentive - Frontend</h1>
+    </>
+  )
+}
+
+export default App
