ab01650139ff61463c767a728fbef18efecd1b2d
oulespaul <oulespaul@gmail.com>
2024-10-03 02:01:26 +0700

    update type

--- src/features/target-commission/components/upload-file-button/components/upload-input.tsx
+++ src/features/target-commission/components/upload-file-button/components/upload-input.tsx
@@ -1,208 +1,197 @@
-import {
-  AudioWaveform,
-  File,
-  FileImage,
-  FolderArchive,
-  UploadCloud,
-  Video,
-  X,
-} from "lucide-react";
-import { useCallback } from "react";
-import { useDropzone } from "react-dropzone";
-import { Input } from "@/components/ui/input";
-import { ScrollArea } from "@/components/ui/scroll-area";
+import { AudioWaveform, File, FileImage, FolderArchive, UploadCloud, Video, X } from 'lucide-react';
+import { useCallback } from 'react';
+import { Input } from '@/components/ui/input';
+import { ScrollArea } from '@/components/ui/scroll-area';
+import { useDropzone } from 'react-dropzone';
 
 export interface FileUploadProgress {
-  progress: number;
-  File: File;
+    progress: number;
+    File: File;
 }
 
 enum FileTypes {
-  Image = "image",
-  Pdf = "pdf",
-  Audio = "audio",
-  Video = "video",
-  Other = "other",
-  Excel = "xls"
+    Image = 'image',
+    Pdf = 'pdf',
+    Audio = 'audio',
+    Video = 'video',
+    Other = 'other',
+    Excel = 'xls',
 }
 
 const ImageColor = {
-  bgColor: "bg-purple-600",
-  fillColor: "fill-purple-600",
+    bgColor: 'bg-purple-600',
+    fillColor: 'fill-purple-600',
 };
 
 const PdfColor = {
-  bgColor: "bg-blue-400",
-  fillColor: "fill-blue-400",
+    bgColor: 'bg-blue-400',
+    fillColor: 'fill-blue-400',
 };
 
 const AudioColor = {
-  bgColor: "bg-yellow-400",
-  fillColor: "fill-yellow-400",
+    bgColor: 'bg-yellow-400',
+    fillColor: 'fill-yellow-400',
 };
 
 const VideoColor = {
-  bgColor: "bg-green-400",
-  fillColor: "fill-green-400",
+    bgColor: 'bg-green-400',
+    fillColor: 'fill-green-400',
 };
 
 const OtherColor = {
-  bgColor: "bg-gray-400",
-  fillColor: "fill-gray-400",
+    bgColor: 'bg-gray-400',
+    fillColor: 'fill-gray-400',
 };
 
 const ExcelColor = {
-  bgColor: "bg-green-400",
-  fillColor: "fill-green-400",
+    bgColor: 'bg-green-400',
+    fillColor: 'fill-green-400',
 };
 
 interface UploadTargetCommissionInputProps {
-  filesToUpload: FileUploadProgress[]
-  setFilesToUpload: React.Dispatch<React.SetStateAction<FileUploadProgress[]>>
+    filesToUpload: FileUploadProgress[];
+    setFilesToUpload: React.Dispatch<React.SetStateAction<FileUploadProgress[]>>;
 }
 
 export default function UploadInput({ filesToUpload, setFilesToUpload }: UploadTargetCommissionInputProps) {
-  const getFileIconAndColor = (file: File) => {
-    if (file.type.includes(FileTypes.Image)) {
-      return {
-        icon: <FileImage size={40} className={ImageColor.fillColor} />,
-        color: ImageColor.bgColor,
-      };
-    }
+    const getFileIconAndColor = (file: File) => {
+        if (file.type.includes(FileTypes.Image)) {
+            return {
+                icon: <FileImage size={40} className={ImageColor.fillColor} />,
+                color: ImageColor.bgColor,
+            };
+        }
 
-    if (file.type.includes(FileTypes.Pdf)) {
-      return {
-        icon: <File size={40} className={PdfColor.fillColor} />,
-        color: PdfColor.bgColor,
-      };
-    }
+        if (file.type.includes(FileTypes.Pdf)) {
+            return {
+                icon: <File size={40} className={PdfColor.fillColor} />,
+                color: PdfColor.bgColor,
+            };
+        }
 
-    if (file.type.includes(FileTypes.Audio)) {
-      return {
-        icon: <AudioWaveform size={40} className={AudioColor.fillColor} />,
-        color: AudioColor.bgColor,
-      };
-    }
+        if (file.type.includes(FileTypes.Audio)) {
+            return {
+                icon: <AudioWaveform size={40} className={AudioColor.fillColor} />,
+                color: AudioColor.bgColor,
+            };
+        }
 
-    if (file.type.includes(FileTypes.Video)) {
-      return {
-        icon: <Video size={40} className={VideoColor.fillColor} />,
-        color: VideoColor.bgColor,
-      };
-    }
+        if (file.type.includes(FileTypes.Video)) {
+            return {
+                icon: <Video size={40} className={VideoColor.fillColor} />,
+                color: VideoColor.bgColor,
+            };
+        }
 
-    if (file.name.includes(FileTypes.Excel)) {
-      return {
-        icon: <File size={40} className={ExcelColor.fillColor} />,
-        color: ExcelColor.bgColor,
-      };
-    }
+        if (file.name.includes(FileTypes.Excel)) {
+            return {
+                icon: <File size={40} className={ExcelColor.fillColor} />,
+                color: ExcelColor.bgColor,
+            };
+        }
 
-    return {
-      icon: <FolderArchive size={40} className={OtherColor.fillColor} />,
-      color: OtherColor.bgColor,
+        return {
+            icon: <FolderArchive size={40} className={OtherColor.fillColor} />,
+            color: OtherColor.bgColor,
+        };
     };
-  };
 
-  const removeFile = (file: File) => {
-    setFilesToUpload((prevUploadProgress) => {
-      return prevUploadProgress.filter((item) => item.File !== file);
-    });
-  };
+    const removeFile = (file: File) => {
+        setFilesToUpload(prevUploadProgress => {
+            return prevUploadProgress.filter(item => item.File !== file);
+        });
+    };
 
-  const onDrop = useCallback(async (acceptedFiles: File[]) => {
-    if (filesToUpload.length >= 1) return;
+    const onDrop = useCallback(
+        async (acceptedFiles: File[]) => {
+            if (filesToUpload.length >= 1) return;
 
-    setFilesToUpload((prevUploadProgress) => {
-      return [
-        ...prevUploadProgress,
-        ...acceptedFiles.map((file) => {
-          return {
-            progress: 0,
-            File: file,
-            source: null,
-          };
-        }),
-      ];
-    });
-  }, [filesToUpload.length]);
+            setFilesToUpload(prevUploadProgress => {
+                return [
+                    ...prevUploadProgress,
+                    ...acceptedFiles.map(file => {
+                        return {
+                            progress: 0,
+                            File: file,
+                            source: null,
+                        };
+                    }),
+                ];
+            });
+        },
+        [filesToUpload.length],
+    );
 
-  const { getRootProps, getInputProps } = useDropzone({ onDrop, disabled: filesToUpload.length === 1 });
+    const { getRootProps, getInputProps } = useDropzone({ onDrop, disabled: filesToUpload.length === 1 });
 
-  return (
-    <div>
-      <div>
-        <label
-          {...getRootProps()}
-          className="relative flex flex-col items-center justify-center w-full py-6 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100 "
-        >
-          <div className="text-center">
-            <div className=" border p-2 rounded-md max-w-min mx-auto">
-              <UploadCloud size={20} />
-            </div>
-
-            <p className="mt-2 text-sm text-gray-600">
-              <span className="font-semibold">Drag files</span>
-            </p>
-            <p className="text-xs text-gray-500">
-              Click to upload files
-            </p>
-            <p className="text-xs text-gray-500">
-              XLSX or XLS
-            </p>
-          </div>
-        </label>
-
-        <Input
-          {...getInputProps()}
-          id="dropzone-file"
-          accept="image/png, image/jpeg"
-          type="file"
-          className="hidden"
-        />
-      </div>
-
-      {filesToUpload.length > 0 && (
+    return (
         <div>
-          <ScrollArea className="h-auto">
-            <p className="font-medium my-2 mt-6 text-muted-foreground text-sm">
-              Files to upload
-            </p>
-            <div className="space-y-2 pr-3">
-              {filesToUpload.map((fileUploadProgress) => {
-                return (
-                  <div
-                    key={fileUploadProgress.File.lastModified}
-                    className="flex justify-between gap-2 rounded-lg overflow-hidden border border-slate-100 group hover:pr-0 pr-2"
-                  >
-                    <div className="flex items-center flex-1 p-2">
-                      <div className="text-white">
-                        {getFileIconAndColor(fileUploadProgress.File).icon}
-                      </div>
-
-                      <div className="w-full ml-2 space-y-1">
-                        <div className="text-sm flex justify-between">
-                          <p className="text-muted-foreground ">
-                            {fileUploadProgress.File.name.slice(0, 25)}
-                          </p>
+            <div>
+                <label
+                    {...getRootProps()}
+                    className="relative flex flex-col items-center justify-center w-full py-6 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100 "
+                >
+                    <div className="text-center">
+                        <div className=" border p-2 rounded-md max-w-min mx-auto">
+                            <UploadCloud size={20} />
                         </div>
-                      </div>
+
+                        <p className="mt-2 text-sm text-gray-600">
+                            <span className="font-semibold">Drag files</span>
+                        </p>
+                        <p className="text-xs text-gray-500">Click to upload files</p>
+                        <p className="text-xs text-gray-500">XLSX or XLS</p>
                     </div>
-                    <button
-                      onClick={() => {
-                        removeFile(fileUploadProgress.File);
-                      }}
-                      className="bg-red-500 text-white transition-all items-center justify-center cursor-pointer px-2 hidden group-hover:flex"
-                    >
-                      <X size={20} />
-                    </button>
-                  </div>
-                );
-              })}
+                </label>
+
+                <Input
+                    {...getInputProps()}
+                    id="dropzone-file"
+                    accept="image/png, image/jpeg"
+                    type="file"
+                    className="hidden"
+                />
             </div>
-          </ScrollArea>
+
+            {filesToUpload.length > 0 && (
+                <div>
+                    <ScrollArea className="h-auto">
+                        <p className="font-medium my-2 mt-6 text-muted-foreground text-sm">Files to upload</p>
+                        <div className="space-y-2 pr-3">
+                            {filesToUpload.map(fileUploadProgress => {
+                                return (
+                                    <div
+                                        key={fileUploadProgress.File.lastModified}
+                                        className="flex justify-between gap-2 rounded-lg overflow-hidden border border-slate-100 group hover:pr-0 pr-2"
+                                    >
+                                        <div className="flex items-center flex-1 p-2">
+                                            <div className="text-white">
+                                                {getFileIconAndColor(fileUploadProgress.File).icon}
+                                            </div>
+
+                                            <div className="w-full ml-2 space-y-1">
+                                                <div className="text-sm flex justify-between">
+                                                    <p className="text-muted-foreground ">
+                                                        {fileUploadProgress.File.name.slice(0, 25)}
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <button
+                                            onClick={() => {
+                                                removeFile(fileUploadProgress.File);
+                                            }}
+                                            className="bg-red-500 text-white transition-all items-center justify-center cursor-pointer px-2 hidden group-hover:flex"
+                                        >
+                                            <X size={20} />
+                                        </button>
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    </ScrollArea>
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 }
